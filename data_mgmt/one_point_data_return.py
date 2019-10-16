aab0ea9aced282eef440797826e4972679cb451f
Will Grant <will.granty@yahoo.co.uk>
2019-10-16 11:54:27 +0100

    code now able to return multiple data queries at once.

--- data_mgmt/one_point_data_return.py
+++ data_mgmt/one_point_data_return.py
@@ -4,9 +4,9 @@ Programme for querying and returning data from master data set.
 
 There are several options that need to be specified below:
 1) returning single data of interest (in to one tab)
-2) returning several data of interest (across multiple tabs)... in development
-3) return data across all masters
-4) return data all pertaining to latest, last and baseline data.
+2) returning several data of interest (across multiple tabs)... in development 
+3) return data across all masters 
+4) return data all pertaining to latest, last and baseline data. 
 
 some formatting is placed into the output file:
 2) changes in reported data - highlighted by salmon pink background,
@@ -25,33 +25,33 @@ from analysis.data import q1_1920, one_quarter_master_list, bespoke_group_master
 from analysis.engine_functions import all_milestone_data_bulk, ap_p_milestone_data_bulk, assurance_milestone_data_bulk,\
     get_all_project_names, get_quarter_stamp, bc_ref_stages, master_baseline_index
 
-def return_data_list(masters_list, project_name_list, data_key_list):
+def return_data(masters_list, project_name_list, data_key_list):
     '''
     places all (non-milestone) data of interest into excel file output
 
     masters_list: list of masters containing quarter information
     project_name_list: list of project to return data for
-    data_key: the data key of interest
+    data_key_list: the data key of interest
 
     '''
 
     salmon_fill = PatternFill(start_color='ff8080', end_color='ff8080', fill_type='solid')
-    # red_text = Font(color="FF0000") #currently not in use
 
     wb = Workbook()
-    ws = wb.active
-
-    '''lists project names in ws'''
-    for x in range(0, len(project_name_list)):
-        try:
-            ws.cell(row=x + 2, column=1).value = masters_list[0].data[project_name_list[x]]['DfT Group']
-        except KeyError:
-            pass
-        ws.cell(row=x + 2, column=2, value=project_name_list[x])
 
     '''project data into ws'''
-    for data in data_key_list:
-        
+    for i, data_key in enumerate(data_key_list):
+        ws = wb.create_sheet(data_key, i)  # creating worksheets
+        ws.title = data_key  # title of worksheet
+
+        '''lists project names in ws'''
+        for x in range(0, len(project_name_list)):
+            try:
+                ws.cell(row=x + 2, column=1).value = masters_list[0].data[project_name_list[x]]['DfT Group']
+            except KeyError:
+                pass
+            ws.cell(row=x + 2, column=2, value=project_name_list[x])
+
 
         for row_num in range(2, ws.max_row + 1):
             project_name = ws.cell(row=row_num, column=2).value
@@ -75,83 +75,25 @@ def return_data_list(masters_list, project_name_list, data_key_list):
                     ws.cell(row=row_num, column=col_start).value = 'Not reporting'
                     col_start += 1
 
-    '''quarter tag / meta data into ws'''
-    quarter_labels = get_quarter_stamp(masters_list)
-    ws.cell(row=1, column=1, value='Group')
-    ws.cell(row=1, column=2, value='Project')
-    for i, label in enumerate(quarter_labels):
-        ws.cell(row=1, column=i + 3, value=label)
+        '''quarter tag / meta data into ws'''
+        quarter_labels = get_quarter_stamp(masters_list)
+        ws.cell(row=1, column=1, value='Group')
+        ws.cell(row=1, column=2, value='Project')
+        for i, label in enumerate(quarter_labels):
+            ws.cell(row=1, column=i + 3, value=label)
 
-    #conditional_formatting(ws)  # apply conditional formatting
-
-    return wb
-
-def data_return(masters_list, project_name_list, data_key):
-    '''
-    places all (non-milestone) data of interest into excel file output
-
-    masters_list: list of masters containing quarter information
-    project_name_list: list of project to return data for
-    data_key: the data key of interest
-
-    '''
-
-    salmon_fill = PatternFill(start_color='ff8080', end_color='ff8080', fill_type='solid')
-    # red_text = Font(color="FF0000") #currently not in use
-
-    wb = Workbook()
-    ws = wb.active
-
-    '''lists project names in ws'''
-    for x in range(0, len(project_name_list)):
-        try:
-            ws.cell(row=x + 2, column=1).value = masters_list[0].data[project_name_list[x]]['DfT Group']
-        except KeyError:
-            pass
-        ws.cell(row=x + 2, column=2, value=project_name_list[x])
-
-    '''project data into ws'''
-    for row_num in range(2, ws.max_row + 1):
-        project_name = ws.cell(row=row_num, column=2).value
-        print(project_name)
-        col_start = 3
-        for i, master in enumerate(masters_list):
-            if project_name in master.projects:
-                try:
-                    ws.cell(row=row_num, column=col_start).value = master.data[project_name][data_key]
-                    if master.data[project_name][data_key] == None:
-                        ws.cell(row=row_num, column=col_start).value = 'None'
-                    try:
-                        if masters_list[i+1].data[project_name][data_key] != master.data[project_name][data_key]:
-                            ws.cell(row=row_num, column=col_start).fill = salmon_fill
-                    except (IndexError, KeyError):
-                        pass
-                    col_start += 1
-                except KeyError:
-                    ws.cell(row=row_num, column=col_start).value = 'data key not collected'
-            else:
-                ws.cell(row=row_num, column=col_start).value = 'Not reporting'
-                col_start += 1
-
-    '''quarter tag / meta data into ws'''
-    quarter_labels = get_quarter_stamp(masters_list)
-    ws.cell(row=1, column=1, value='Group')
-    ws.cell(row=1, column=2, value='Project')
-    for i, label in enumerate(quarter_labels):
-        ws.cell(row=1, column=i + 3, value=label)
-
-    #conditional_formatting(ws)  # apply conditional formatting
+        grey_conditional_formatting(ws)  # apply grey formatting
 
     return wb
 
-def data_return_baseline(masters_list, baseline_list, baseline_ref, project_name_list, data_key):
+def return_baseline_data(masters_list, baseline_list, baseline_ref, project_name_list, data_key_list):
     '''
     places all non-milestone data into output document with latest, last and baseline data. Also states which quarter
     is being used as baseline
     :param masters_list: list of master quarter data
     :param baseline_list: list indexing where latest, last and baseline master data for each project
     :param project_name_list: list of project names
-    :param data_key: data of interest/to return
+    :param data_key_list: data of interest/to return
     :return: excel spreadsheet
     '''
 
@@ -159,52 +101,52 @@ def data_return_baseline(masters_list, baseline_list, baseline_ref, project_name
     # red_text = Font(color="FF0000") #currently not in use
 
     wb = Workbook()
-    ws = wb.active
-
-    '''lists project names in ws'''
-    for x in range(0, len(project_name_list)):
-        try:
-            ws.cell(row=x + 2, column=1).value = masters_list[0].data[project_name_list[x]]['DfT Group']
-        except KeyError:
-            pass
-        ws.cell(row=x + 2, column=2, value=project_name_list[x])
 
     '''project data into ws'''
-    for row_num in range(2, ws.max_row + 1):
-        project_name = ws.cell(row=row_num, column=2).value
-        ws.cell(row=row_num, column=8).value = baseline_ref[project_name][2][0] # ref to baseline quarter
-        print(project_name)
-        col_start = 3
-        for i in baseline_list[project_name]:
+    for i, data_key in enumerate(data_key_list):
+        ws = wb.create_sheet(data_key, i)  # creating worksheets
+        ws.title = data_key  # title of worksheet
+
+        '''lists project names in ws'''
+        for x in range(0, len(project_name_list)):
             try:
-                ws.cell(row=row_num, column=col_start).value = masters_list[i].data[project_name][data_key]
-                if masters_list[i].data[project_name][data_key] == None:
-                    ws.cell(row=row_num, column=col_start).value = 'None'
-                try:
-                    if masters_list[i+1].data[project_name][data_key] != masters_list[i].data[project_name][data_key]:
-                        ws.cell(row=row_num, column=col_start).fill = salmon_fill
-                except (IndexError, KeyError):
-                    pass
-                col_start += 1
+                ws.cell(row=x + 2, column=1).value = masters_list[0].data[project_name_list[x]]['DfT Group']
             except KeyError:
-                ws.cell(row=row_num, column=col_start).value = 'Data not collected'
-                col_start += 1
+                pass
+            ws.cell(row=x + 2, column=2, value=project_name_list[x])
 
-    '''quarter tag / meta data into ws'''
-    baseline_labels = ['This quarter', 'Last quarter', 'Baseline quarter']
-    ws.cell(row=1, column=1, value='Group')
-    ws.cell(row=1, column=2, value='Project')
-    for i, label in enumerate(baseline_labels):
-        ws.cell(row=1, column=i + 3, value=label)
-    ws.cell(row=1, column=8, value='Quarter from which baseline data taken')
+        '''project data into ws'''
+        for row_num in range(2, ws.max_row + 1):
+            project_name = ws.cell(row=row_num, column=2).value
+            ws.cell(row=row_num, column=8).value = baseline_ref[project_name][2][0] # ref to baseline quarter
+            print(project_name)
+            col_start = 3
+            for i in baseline_list[project_name]:
+                try:
+                    ws.cell(row=row_num, column=col_start).value = masters_list[i].data[project_name][data_key]
+                    if masters_list[i].data[project_name][data_key] == None:
+                        ws.cell(row=row_num, column=col_start).value = 'None'
+                    try:
+                        if masters_list[i+1].data[project_name][data_key] != masters_list[i].data[project_name][data_key]:
+                            ws.cell(row=row_num, column=col_start).fill = salmon_fill
+                    except (IndexError, KeyError):
+                        pass
+                    col_start += 1
+                except KeyError:
+                    ws.cell(row=row_num, column=col_start).value = 'Data not collected'
+                    col_start += 1
 
-
-    #conditional_formatting(ws)  # apply conditional formatting
+        '''quarter tag / meta data into ws'''
+        baseline_labels = ['This quarter', 'Last quarter', 'Baseline quarter']
+        ws.cell(row=1, column=1, value='Group')
+        ws.cell(row=1, column=2, value='Project')
+        for i, label in enumerate(baseline_labels):
+            ws.cell(row=1, column=i + 3, value=label)
+        ws.cell(row=1, column=8, value='Quarter from which baseline data taken')
 
     return wb
 
-
-def milestone_data_return(masters_list, project_name_list, data_key):
+def return_milestone_data(masters_list, project_name_list, data_key):
     ''' places all milestone data of interest into excel file output
 
     master_list: list of masters containing quarter information
@@ -270,12 +212,10 @@ def milestone_data_return(masters_list, project_name_list, data_key):
     return wb
 
 def conditional_formatting(worksheet):
-
     '''
-    Function for applying rag rating conditional formatting colouring.
-
-    This doesn't always need to be applied. ToDo: Need to think/design and way of placing this into instructions.
-
+    function applie conditional formating for RAG colors... in development.
+    :param worksheet:
+    :return:
     '''
 
     ag_text = Font(color="000000")
@@ -313,6 +253,15 @@ def conditional_formatting(worksheet):
     rule.formula = ['NOT(ISERROR(SEARCH("Amber",A1)))']
     worksheet.conditional_formatting.add('A1:X80', rule)
 
+    return worksheet
+
+def grey_conditional_formatting(worksheet):
+    '''
+    function applies grey conditional formatting for 'Not Reporting'.
+    :param worksheet: ws
+    :return: cf of sheet
+    '''
+
     grey_text = Font(color="f0f0f0")
     grey_fill = PatternFill(bgColor="f0f0f0")
     dxf = DifferentialStyle(font=grey_text, fill=grey_fill)
@@ -320,16 +269,7 @@ def conditional_formatting(worksheet):
     rule.formula = ['NOT(ISERROR(SEARCH("Not reporting",A1)))']
     worksheet.conditional_formatting.add('A1:X80', rule)
 
-    # highlighting new projects
-    red_text = Font(color="000000")
-    white_fill = PatternFill(bgColor="000000")
-    dxf = DifferentialStyle(font=red_text, fill=white_fill)
-    rule = Rule(type="containsText", operator="containsText", text="NEW", dxf=dxf)
-    rule.formula = ['NOT(ISERROR(SEARCH("NEW",A1)))']
-    worksheet.conditional_formatting.add('A1:X80', rule)
-
     return worksheet
-
 
 ''' RUNNING PROGRAMME '''
 
@@ -348,8 +288,9 @@ baseline_list = master_baseline_index(one_quarter_list, list_of_masters_all, bas
 
 '''THREE. Set data of interest. there are two options here. hash out whichever option you are not using'''
 
-'''option one - non-milestone data'''
-data_interest = 'Total Forecast'
+'''option one - non-milestone data. NOTE. this must be in a list even if just one data key'''
+data_interest = ['Total Forecast']
+                 #'Present Value Cost (PVC)']
 
 '''option two - milestone data'''
 #milestone_data_interest = 'Project End Date'
@@ -357,13 +298,13 @@ data_interest = 'Total Forecast'
 '''THREE. Run the programme'''
 
 '''option one - run the data_return function for all non-milestone data'''
-#run = data_return(list_of_masters_all, one_quarter_list, data_interest)
+run = return_data(list_of_masters_all, one_quarter_list, data_interest)
 
 '''option two - run the data_return_baseline function for all non-milestone data'''
-run = data_return_baseline(list_of_masters_all, baseline_list, baseline_bc, one_quarter_list, data_interest)
+#run = return_baseline_data(list_of_masters_all, baseline_list, baseline_bc, one_quarter_list, data_interest)
 
 '''option two - run the milestone_data_return for all milestone data'''
 #run = milestone_data_return(list_of_masters_all, one_quarter_list, milestone_data_interest)
 
 '''FOUR. specify the file path and name of the output document'''
-run.save('C:\\Users\\Standalone\\general\\testing_baseline_output.xlsx')
+run.save('C:\\Users\\Standalone\\general\\testing_greying_out.xlsx')
