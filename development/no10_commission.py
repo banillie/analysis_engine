80a987bca343b2439f246091a8e6c570e4922168
me <will.granty@yahoo.co.uk>
2020-02-14 12:07:12 +0000

    further coding for no10_commission.py

--- development/no10_commission.py
+++ development/no10_commission.py
@@ -22,13 +22,34 @@ def return_baseline_data(project_name_list, data_key_list):
 
         '''list project names, groups and stage in ws'''
         for y, key in enumerate(data_key_list):
-            ws.cell(row=2+y, column=1, value=key) #returns key
-            ws.cell(row=2+y, column=2).value = list_of_masters_all[0].data[project_name][key] #returns latest value
-            for x in range(0, len(baseline_bc_stamp[project_name])):
-                quarter_info = baseline_bc_stamp[project_name][x][1]
-                ws.cell(row=1, column=3+x, value=quarter_info) #returns quarter info
-                index = baseline_bc_stamp[project_name][x][2]
-                ws.cell(row=2+y, column=3+x, value=list_of_masters_all[index].data[project_name][key]) #returns baslines
+            if key in list_of_masters_all[0].data[project_name].keys():
+                #standard keys
+                ws.cell(row=2+y, column=1, value=key) #returns key
+                ws.cell(row=2+y, column=2).value = list_of_masters_all[0].data[project_name][
+                    key] #returns latest value
+                for x in range(0, len(baseline_bc_stamp[project_name])):
+                    quarter_info = baseline_bc_stamp[project_name][x][1]
+                    ws.cell(row=1, column=3+x, value=quarter_info) #returns quarter info
+                    index = baseline_bc_stamp[project_name][x][2]
+                    ws.cell(row=2+y, column=3+x, value=list_of_masters_all[index].data[project_name][key]) #returns baselines
+            else:
+                #milestones keys
+                ws.cell(row=2 + y, column=1, value=key)  # returns key
+                milestones = all_milestone_data_bulk([project_name], list_of_masters_all[0])
+                try:
+                    ws.cell(row=2 + y, column=2).value = tuple(milestones[project_name][key])[0]  # returns latest value
+                except KeyError:
+                    ws.cell(row=2 + y, column=2).value = 'missing data'
+
+                for x in range(0, len(baseline_bc_stamp[project_name])):
+                    quarter_info = baseline_bc_stamp[project_name][x][1]
+                    ws.cell(row=1, column=3 + x, value=quarter_info)  # returns quarter info
+                    index = baseline_bc_stamp[project_name][x][2]
+                    try:
+                        milestones = all_milestone_data_bulk([project_name], list_of_masters_all[index])
+                        ws.cell(row=2 + y, column=3 + x).value = tuple(milestones[project_name][key])[0]  # returns baselines
+                    except KeyError:
+                        ws.cell(row=2 + y, column=3 + x).value = 'project not reporting'
 
         ws.cell(row=1, column=1, value='Key')
         ws.cell(row=1, column=2, value='Latest')
@@ -50,16 +71,33 @@ def return_data(project_name_list, data_key_list):
             ws.cell(row=2+y, column=1, value=key) #returns key
             for x, master in enumerate(list_of_masters_all):
 
-                try:
-                    value = master.data[project_name][key]
-                    ws.cell(row=2+y, column=2+x, value=value) #retuns value
+                #standard keys
 
-                    try:  #loop checks if value has changed since last quarter
-                        lst_value = list_of_masters_all[x+1].data[project_name][key]
-                        if value != lst_value:
-                            ws.cell(row=2+y, column=2+x).fill = salmon_fill
-                    except IndexError:
-                        pass
+                try:
+                    #standard keys
+                    if key in master.data[project_name].keys():
+                        value = master.data[project_name][key]
+                        ws.cell(row=2+y, column=2+x, value=value) #retuns value
+
+                        try:  #loop checks if value has changed since last quarter
+                            lst_value = list_of_masters_all[x+1].data[project_name][key]
+                            if value != lst_value:
+                                ws.cell(row=2+y, column=2+x).fill = salmon_fill
+                        except IndexError:
+                            pass
+
+                    # milestone keys
+                    else:
+                        milestones = all_milestone_data_bulk([project_name], list_of_masters_all[x])
+                        value = tuple(milestones[project_name][key])[0]
+                        ws.cell(row=2 + y, column=2 + x, value=value)
+                        try:  # loop checks if value has changed since last quarter
+                            old_milestones = all_milestone_data_bulk([project_name], list_of_masters_all[x + 1])
+                            lst_value = tuple(old_milestones[project_name][key])[0]
+                            if value != lst_value:
+                                ws.cell(row=2 + y, column=2 + x).fill = salmon_fill
+                        except IndexError:
+                            pass
 
                 except KeyError:
                     if project_name in master.projects: #loop calculates if project was not reporting or data missing
@@ -91,18 +129,26 @@ def return_data(project_name_list, data_key_list):
 list_columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'q', 's', 't', 'u', 'w']
 conditional_text = ['missing data', 'project not reporting']
 '''store of different colours'''
-reddish_text = Font(color="00CC4833")
-reddish_fill = PatternFill(bgColor="00CC4833")
-grey_text = Font(color="00D2A099")
-grey_fill = PatternFill(bgColor="00D2A099")
-text_colours = [reddish_text, grey_text]
-background_colours = [reddish_fill, grey_fill]
+darkish_grey_text = Font(color="00CC4833")
+darkish_grey_fill = PatternFill(bgColor="00CC4833")
+light_grey_text = Font(color="0085929e")
+light_grey_fill = PatternFill(bgColor="0085929e")
+text_colours = [darkish_grey_text, light_grey_text]
+background_colours = [darkish_grey_fill, light_grey_fill]
 
 
 
 
 '''data keys of interest'''
-milestone_data_interest = ['BICC approval point', 'Total Forecast']
+milestone_data_interest = ['BICC approval point',
+                           'Total Forecast',
+                           'Project End Date',
+                           'Departmental DCA',
+                           'Initial Benefits Cost Ratio (BCR)',
+                           'Adjusted Benefits Cost Ratio (BCR)',
+                           'VfM Category single entry',
+                           'VfM Category upper range',
+                           'VfM Category lower range']
 
 '''running prog - step one'''
 run_project_all = return_data(latest_quarter_project_names, milestone_data_interest)
