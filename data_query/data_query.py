1ccdd179e63f3f4e4bd45bc33d032562596664c8
Will Grant <will.granty@yahoo.co.uk>
2020-04-15 16:50:55 +0100

    changes in salmon fill and conditional formatting of grey cells fixed

--- data_query/data_query.py
+++ data_query/data_query.py
@@ -52,17 +52,17 @@ def return_data(project_name_list, data_key_list):
                         #standard keys
                         if key in list_of_masters_all[x].data[project_name].keys():
                             value = list_of_masters_all[x].data[project_name][key]
-                            ws.cell(row=2 + y, column=3 + x, value=value) #returns value
+                            ws.cell(row=2 + y, column=3 + x, value=value) # returns value
 
                             if value is None:
                                 ws.cell(row=2 + y, column=3 + x, value='md')
 
-                        try: # checks for change against last quarter
-                            lst_value = list_of_masters_all[x + 1].data[project_name][key]
-                            if value != lst_value:
-                                ws.cell(row=2 + y, column=3 + x).fill = salmon_fill
-                        except (KeyError, IndexError):
-                            pass
+                            try: # checks for change against last quarter
+                                lst_value = list_of_masters_all[x + 1].data[project_name][key]
+                                if value != lst_value:
+                                    ws.cell(row=2 + y, column=3 + x).fill = salmon_fill
+                            except (KeyError, IndexError):
+                                pass
 
                         # milestone keys
                         else:
@@ -98,7 +98,7 @@ def return_data(project_name_list, data_key_list):
         for l, label in enumerate(quarter_labels):
             ws.cell(row=1, column=l + 3, value=label)
 
-        list_columns = list_column_ltrs[2:len(list_of_masters_all)+4]
+        list_columns = list_column_ltrs[2:len(list_of_masters_all)+2]
 
         if key in list_of_rag_keys:
             conditional_formatting(ws, list_columns, rag_txt_list_full, rag_txt_colours, rag_fill_colours, '1', '60')
@@ -176,16 +176,7 @@ def return_baseline_data(project_name_list, data_key_list):
     return wb
 
 '''data keys of interest. Place all keys of interest as stings in this list'''
-data_interest = ['NPV for all projects and NPV for programmes if available',
-                 'Adjusted Benefits Cost Ratio (BCR)',
-                 'Initial Benefits Cost Ratio (BCR)',
-                 'Present Value Cost (PVC)',
-                 'Present Value Benefit (PVB)',
-                 'VfM Category single entry',
-                 'VfM Category lower range',
-                 'VfM Category upper range',
-                 'Benefits Narrative',
-                 'Start of Operation']
+data_interest = ['Total Forecast']
 
 '''Running the programme'''
 
@@ -202,8 +193,8 @@ second variable = data_interest. This name does not change. List compiled above'
 run_baseline = return_baseline_data(list_of_masters_all[0].projects, data_interest)
 
 '''Specify name of the output document here. See general guidance re saving output files'''
-run_standard.save(root_path/'output/vfm_data_q4_2019.xlsx')
-run_baseline.save(root_path/'output/vfm_data_bl_q4_2019.xlsx')
+run_standard.save(root_path/'output/data_query_return.xlsx')
+run_baseline.save(root_path/'output/data_query_return_bl.xlsx')
 
 '''old lists stored here for use in future'''
 project_basics = ['Brief project description (GMPP - brief descripton)',
@@ -226,6 +217,17 @@ vfm_analysis_list = ['Departmental DCA',
                      'SRO Benefits RAG',
                      'Benefits Narrative',
                      'Ben comparison with last quarters cost - narrative']
+
+vfm_analysis_list_two = ['NPV for all projects and NPV for programmes if available',
+                 'Adjusted Benefits Cost Ratio (BCR)',
+                 'Initial Benefits Cost Ratio (BCR)',
+                 'Present Value Cost (PVC)',
+                 'Present Value Benefit (PVB)',
+                 'VfM Category single entry',
+                 'VfM Category lower range',
+                 'VfM Category upper range',
+                 'Benefits Narrative',
+                 'Start of Operation']
 
 ipa_ar_fields_1920 =  ['Department',
                        '19-20 RDEL BL Total',
