a59e6c74e5ebefe63337d7f747d75dd5de597d6a
Will Grant <will.granty@yahoo.co.uk>
2020-04-23 10:18:37 +0100

    programme now changes all master data

--- milestone_analysis/change_milestone_keys.py
+++ milestone_analysis/change_milestone_keys.py
@@ -26,9 +26,8 @@ def change_key(project_list, master_wb_title_list, change_key):
 
     for name in project_list:
         #print(name)
-        for index in bc_index[name]:
-            print(index)
-            wb = load_workbook(master_wb_title_list[index])
+        for master in master_wb_title_list:
+            wb = load_workbook(master)
             ws = wb.active
             for col_num in range(2, ws.max_column + 1):
                 project_name = ws.cell(row=1, column=col_num).value
@@ -44,13 +43,14 @@ def change_key(project_list, master_wb_title_list, change_key):
                             else:
                                 pass
 
-            wb.save(master_wb_title_list[index])
+            wb.save(master)
 
 
 '''INSTRUCTIONS FOR RUNNING THE PROGRAMME'''
 
 '''ONE. List of file paths to masters'''
-master_list = (root_path/'core_data/master_3_2019.xlsx',
+master_list = (root_path/'core_data/master_4_2019.xlsx',
+               root_path/'core_data/master_3_2019.xlsx',
                root_path/'core_data/master_2_2019.xlsx',
                root_path/'core_data/master_1_2019.xlsx',
                root_path/'core_data/master_4_2018.xlsx',
@@ -65,7 +65,7 @@ master_list = (root_path/'core_data/master_3_2019.xlsx',
                root_path/'core_data/master_3_2016.xlsx')
 
 '''TWO. Provide file path to document which contains information on the data that needs to be changed'''
-key_change = project_data_from_master(root_path/'input/change_milestone_keys_q3_1920.xlsx', 2, 2019)
+key_change = project_data_from_master(root_path/'input/change_milestone_keys_q4_1920.xlsx', 2, 2019)
 
 '''THREE. List of projects. taken from the key change document - as this contains the only projects that need 
 information changed'''
