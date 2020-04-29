ad439fe20abae567f10d56cb3510dcf4bbece612
Will Grant <will.granty@yahoo.co.uk>
2020-04-29 12:34:14 +0100

    update for new reporting master/framework

--- covid/covid_compare_txt.py
+++ covid/covid_compare_txt.py
@@ -5,6 +5,8 @@ flags changes to text in red
 requirements:
 1) covid master wbs to go into analysis_engine core data directory
 2) output file goes into output directory
+
+follow instructions below.
 '''
 
 
@@ -13,14 +15,11 @@ from analysis.engine_functions import compare_text_newandold
 from analysis.data import root_path
 from docx import Document
 
-master_17_4_20 = project_data_from_master(root_path/'core_data/master_170420.xlsx', 1, 2020)
-master_2_4_20 = project_data_from_master(root_path/'core_data/master_020420.xlsx', 1, 2020)
-
 def printing():
 
      doc = Document()
 
-     for project_name in master_17_4_20.projects:
+     for project_name in latest_master.projects:
          heading = str(project_name)
          intro = doc.add_heading(str(heading), 0)
          intro.alignment = 1
@@ -29,19 +28,46 @@ def printing():
          new = doc.add_paragraph()
          new.add_run('')
 
-         latest_txt = master_17_4_20.data[project_name]['Narrative']
-         last_txt = master_2_4_20.data[project_name]['Narrative']
+
+         latest_txt = latest_master.data[project_name]['Project Narrative']
+         if latest_txt is None:
+             latest_txt = latest_master.data[project_name]['Group Narrative']
 
          try:
-            compare_text_newandold(latest_txt, last_txt, doc)
-         except AttributeError:
-             pass
+             last_txt = last_master.data[project_name]['Project Narrative']
+             if last_txt is None:
+                 last_txt = last_master.data[project_name]['Group Narrative']
 
-         new_2 = doc.add_paragraph()
-         new_2.add_run('')
+             try:
+                compare_text_newandold(latest_txt, last_txt, doc)
+             except AttributeError:
+                 pass
+
+             new_2 = doc.add_paragraph()
+             new_2.add_run('')
+
+         except KeyError:
+             pass
+             # new_3 = doc.add_paragraph
+             # new_3.add_run('First time project reporting')
+             # new_4 = doc.add_paragraph
+             # new_4.add_run(latest_txt)
+             # new_5 = doc.add_paragraph
+             # new_5.add_run('')
+
 
      return doc
 
+'''RUNNING PROGRAMME INSTRUCTIONS'''
+
+'''Insert into the two file paths below the name of latest and last master, after core_data/ .
+Example master file name is master_170420.xlsx. Note make sure .xlsx is at end of file name. 
+The rest of the file path information can remain unchanged'''
+latest_master = project_data_from_master(root_path/'core_data/master_170420.xlsx', 1, 2020)
+last_master = project_data_from_master(root_path/'core_data/master_020420.xlsx', 1, 2020)
+
 run = printing()
 
+'''The name of the output document can be changed via changing the file path here, if desired. The standard
+name output name is covid_compare_text. Note make sure .docx is at end'''
 run.save(root_path/'output/covid_compare_text.docx')
