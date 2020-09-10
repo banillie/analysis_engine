2f6fac0fcef9b1fcaf8e2735cfe6e5a037fd7d91
Will Grant <will.granty@yahoo.co.uk>
2020-09-10 13:48:28 +0100

    update inline with MilestoneData class changes. More generic user handling

--- milestone_analysis/milestones_comparison_3_qrts_all.py
+++ milestone_analysis/milestones_comparison_3_qrts_all.py
@@ -15,35 +15,35 @@ def put_into_wb_all(milestone_data_object):
 
     row_num = 2
     for project_name in milestone_data_object.project_current.keys():
-        for i, milestone in enumerate(milestone_data_object.project_current_p_project[project_name].keys()):
+        for i, milestone in enumerate(milestone_data_object.project_current[project_name].keys()):
             ws.cell(row=row_num + i, column=1).value = project_name
             ws.cell(row=row_num + i, column=2).value = milestone
             try:
-                milestone_date = tuple(milestone_data_object.project_current_p_project[project_name][milestone])[0]
+                milestone_date = tuple(milestone_data_object.project_current[project_name][milestone])[0]
                 ws.cell(row=row_num + i, column=3).value = milestone_date
                 ws.cell(row=row_num + i, column=3).number_format = 'dd/mm/yy'
             except KeyError:
                 ws.cell(row=row_num + i, column=3).value = ''
 
             try:
-                last_date = tuple(milestone_data_object.project_last_p_project[project_name][milestone])[0]
+                last_date = tuple(milestone_data_object.project_last[project_name][milestone])[0]
                 ws.cell(row=row_num + i, column=4).value = (milestone_date - last_date).days
             except (KeyError, TypeError):
                 ws.cell(row=row_num + i, column=4).value = ''
 
             try:
-                baseline_date = tuple(milestone_data_object.project_baseline_p_project[project_name][milestone])[0]
+                baseline_date = tuple(milestone_data_object.project_baseline[project_name][milestone])[0]
                 ws.cell(row=row_num + i, column=5).value = (milestone_date - baseline_date).days
             except (KeyError, TypeError):
                 ws.cell(row=row_num + i, column=5).value = ''
 
             try:
-                notes = milestone_data_object.project_current_p_project[project_name][milestone][milestone_date]
+                notes = milestone_data_object.project_current[project_name][milestone][milestone_date]
                 ws.cell(row=row_num + i, column=7).value = notes
             except (IndexError, KeyError):
                 ws.cell(row=row_num + i, column=7).value = ''
 
-        row_num = row_num + len(milestone_data_object.project_current_p_project[project_name].keys())
+        row_num = row_num + len(milestone_data_object.project_current[project_name].keys())
 
     ws.cell(row=1, column=1).value = 'Project'
     ws.cell(row=1, column=2).value = 'Milestone'
@@ -55,9 +55,10 @@ def put_into_wb_all(milestone_data_object):
 
     return wb
 
-mst = Masters(master_data_list, Projects.current_list)  # get master data and specify projects
-mst.baseline_data('Re-baseline IPDC milestones')  # get baseline information of interest
-milestone_data = MilestoneData(mst, abbreviations)  # get milestone data
+mst = Masters(master_data_list, Projects.current_list)  # get Master object and specify projects of interest
+mst.baseline_data('Re-baseline IPDC milestones')  # place baseline information of interest into master object
+milestone_data = MilestoneData(mst, abbreviations)  # create MilestoneData object
+milestone_data.project_data('Delivery') # place type of milestone data of interest into MilestoneData object
 
 run = put_into_wb_all(milestone_data)
-run.save(root_path/"output/all_delivery_milestones_q1_20_21.xlsx")
+run.save(root_path/"output/milestone_data_output.xlsx")
