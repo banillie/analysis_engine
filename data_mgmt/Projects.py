bc5589cb5120256ce3934f87d5af0d08d18cda39
Will Grant <will.granty@yahoo.co.uk>
2020-09-12 21:24:03 +0100

    First commit

---
+++ data_mgmt/Projects.py
@@ -0,0 +1,68 @@
+# project names as variables
+a12 = 'A12 Chelmsford to A120 widening'
+a14 = 'A14 Cambridge to Huntingdon Improvement Scheme'
+a303 = 'A303 Amesbury to Berwick Down'
+a385 = 'A358 Taunton to Southfields Dualling'
+a417 = 'A417 Air Balloon'
+a428 = 'A428 Black Cat to Caxton Gibbet'
+a66 = 'A66 Northern Trans-Pennine'
+brighton_ml = 'Brighton Mainline Upgrade Upgrade Programme (BMUP)'
+cvs = 'Commercial Vehicle Services (CVS)'
+east_coast_digital = 'East Coast Digital Programme'
+east_coast_mainline = 'East Coast Mainline Programme'
+em_franchise = 'East Midlands Franchise'
+ewr_central = 'East West Rail Programme (Central Section)'
+ewr_western = 'East West Rail Programme (Western Section)'
+ftts = 'Future Theory Test Service (FTTS)'
+heathrow_expansion = 'Heathrow Expansion'
+hexagon = 'Hexagon'
+hs2_programme = 'High Speed Rail Programme (HS2)'
+hs2_2b = 'HS2 Phase 2b'
+hs2_1 = 'HS2 Phase1'
+hs2_2a = 'HS2 Phase2a'
+ist = 'Integrated and Smart Ticketing - creating an account based back office'
+lower_thames_crossing = 'Lower Thames Crossing'
+m4 = 'M4 Junctions 3 to 12 Smart Motorway'
+manchester_north_west_quad = 'Manchester North West Quadrant'
+midland_mainline = 'Midland Main Line Programme'
+midlands_rail_hub = 'Midlands Rail Hub'
+north_of_england = 'North of England Programme'
+northern_powerhouse = 'Northern Powerhouse Rail'
+nwe = 'North Western Electrification'
+ox_cam_expressway = 'Oxford-Cambridge Expressway'
+rail_franchising = 'Rail Franchising Programme'
+west_coast_partnership = 'West Coast Partnership Franchise'
+crossrail = 'Crossrail Programme'
+gwrm = 'Great Western Route Modernisation (GWRM) including electrification'
+iep = 'Intercity Express Programme'
+sarh2 = '2nd Generation UK Search and Rescue Aviation'
+south_west_route_capacity = 'South West Route Capacity'
+thameslink = 'Thameslink Programme'
+tru = 'Transpennine Route Upgrade (TRU)'
+wrlth = 'Western Rail Link to Heathrow'
+
+#lists of projects names in groups
+rpe = [lower_thames_crossing,
+       a303,
+       a14,
+       a66,
+       a12,
+       m4,
+       a428,
+       a417,
+       a385,
+       ftts,
+       ist]
+hs2 = [hs2_1, hs2_2a, hs2_2b]
+fbc_stage = [hs2_1,
+       crossrail,
+       east_coast_mainline,
+       iep,
+       thameslink,
+       south_west_route_capacity,
+       hexagon,
+       gwrm,
+       nwe,
+       midland_mainline,
+       m4,
+       a14]
