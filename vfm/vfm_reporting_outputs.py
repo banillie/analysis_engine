ad2fd404107fbd79ff6f6c95b9f201763a3e118c
Will Grant <will.granty@yahoo.co.uk>
2020-09-21 14:47:25 +0100

    first commit

---
+++ vfm/vfm_reporting_outputs.py
@@ -0,0 +1,15 @@
+#  code of running and compiling vfm data analysis each quarter.
+
+from data_mgmt.data import root_path
+from vfm.database import convert_db_python_dict, get_project_names
+from vfm.vfm_analysis_workings import compile_data_db
+
+q_list = ['q1_2021', 'q4_1920']
+db_path = root_path / "core_data/vfm.db"
+master_dict = convert_db_python_dict(db_path, q_list)
+project_names = get_project_names(db_path, 'q1_2021')
+ordered_cat_list = ['Poor', 'Low', 'Medium', 'High', 'Very High',
+                    'Very High and Financially Positive', 'Economically Positive',
+                    None]
+run = compile_data_db(master_dict, project_names, ordered_cat_list)
+run.save(root_path / "output/vfm_data_wb_output.xlsx")
