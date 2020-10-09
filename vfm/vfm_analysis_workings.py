6e49f39c6a52dacf2069a4213ffbd5ff53d30309
Will Grant <will.granty@yahoo.co.uk>
2020-10-09 16:53:12 +0100

    more prep for quarter two reporting

--- vfm/vfm_analysis_workings.py
+++ vfm/vfm_analysis_workings.py
@@ -7,7 +7,7 @@ from data_mgmt.data import vfm_matplotlib_graph
 
 
 #  Places data in excel wb. Using python dictionary structure.
-def compile_data(masters, project_name_list):
+def compile_data(masters, project_name_list, cat_list):
     wb = Workbook()
     ws = wb.active
 
@@ -55,25 +55,16 @@ def compile_data(masters, project_name_list):
     ws.cell(row=1, column=5).value = 'Adjusted BCR'
     ws.cell(row=1, column=6).value = 'Adjusted BCR lst qrt'
     ws.cell(row=1, column=7).value = 'Initial BCR'
-    try:
-        ws.cell(row=i, column=8).value = masters[1].data[project]['Initial Benefits Cost Ratio (BCR)']
-    except KeyError:
-        ws.cell(row=i, column=8).value = 'Not reporting'
-    ws.cell(row=i, column=9).value = masters[0].data[project]['VfM Category single entry']
-    try:
-        ws.cell(row=i, column=10).value = masters[1].data[project]['VfM Category single entry']
-    except KeyError:
-        ws.cell(row=i, column=10).value = 'Not reporting'
-    ws.cell(row=i, column=11).value = masters[0].data[project]['Present Value Cost (PVC)']
-    try:
-        ws.cell(row=i, column=12).value = masters[1].data[project]['Present Value Cost (PVC)']
-    except KeyError:
-        ws.cell(row=i, column=12).value = 'Not reporting'
-    ws.cell(row=i, column=13).value = masters[0].data[project]['Present Value Benefit (PVB)']
-    try:
-        ws.cell(row=i, column=14).value = masters[1].data[project]['Present Value Benefit (PVB)']
-    except KeyError:
-        ws.cell(row=i, column=14).value = 'Not reporting'
+    ws.cell(row=1, column=8).value = 'Initial BCR lst qrt'
+    ws.cell(row=1, column=9).value = 'VfM Category'
+    ws.cell(row=1, column=10).value = 'VfM Category lst qrt'
+    ws.cell(row=1, column=11).value = 'PVC'
+    ws.cell(row=1, column=12).value = 'PVC lst qrt'
+    ws.cell(row=1, column=13).value = 'PVB'
+    ws.cell(row=1, column=14).value = 'PVB lst qrt'
+
+    compile_vfm_cat_data_db(wb, masters, cat_list, 36)
+    calculate_pvc(wb, masters, cat_list, 48)
 
     return wb
 
@@ -180,31 +171,30 @@ def compile_data_pure_db(db_name, project_names, key_list, column_index):
 
 #  places vfm category figures into excel wb
 def compile_vfm_cat_data_db(wb, masters, cat_list, start_row):
-    #wb = Workbook()
     ws = wb.active
 
     latest_qrt_graph = []
     lst_qrt_graph = []
     row = start_row + 1
     for x, cat in enumerate(cat_list):
-        for col, m in enumerate(masters):
+        for m, master in enumerate(masters[0:2]):
             counter = 0
             total_projects = 0
-            project_name = list(masters[m].keys())
+            project_name = list(masters[m].data.keys())
             for p in project_name:
-                if masters[m][p]['vfm_cat_single'] == cat:
+                if masters[m].data[p]["VfM Category single entry"] == cat:
                     counter += 1
                 total_projects += 1
 
-            ws.cell(row=start_row, column=2 + col).value = m  # values entered from col 2 onwards
-            ws.cell(row=row+x, column=2 + col).value = counter  # values entered from col 2 onwards
+            ws.cell(row=start_row, column=2 + m).value = str(master.quarter)  # values entered from col 2 onwards
+            ws.cell(row=row+x, column=2 + m).value = counter  # values entered from col 2 onwards
 
-            ws.cell(row=start_row + len(cat_list) + 1, column=2 + col).value = total_projects
+            ws.cell(row=start_row + len(cat_list) + 1, column=2 + m).value = total_projects
 
             #  for the graph
-            if col == 0:
+            if m == 0:
                 latest_qrt_graph.append(counter)
-            if col == 1:
+            if m == 1:
                 lst_qrt_graph.append(counter)
 
         if cat is not None:
@@ -227,26 +217,29 @@ def calculate_pvc(wb, masters, cat_list, start_row):
     lst_qrt_graph = []
     row = start_row + 1
     for x, cat in enumerate(cat_list):
-        for i, m in enumerate(masters):
+        for m, master in enumerate(masters[0:2]):
             total = 0
-            projects = (masters[m].keys())
+            projects = (masters[m].data.keys())
             for p in projects:
-                if masters[m][p]['vfm_cat_single'] == cat:
-                    value = masters[m][p]['pvc']
+                if masters[m].data[p]["VfM Category single entry"] == cat:
+                    value = masters[m].data[p]['Present Value Cost (PVC)']
                     if value is not None:
-                        total += value
+                        try:
+                            total += value
+                        except TypeError:
+                            pass
                     else:
                         pass
                 else:
                     pass
 
-            ws.cell(row=row + x, column=i + 2).value = total
-            ws.cell(row=start_row, column=i + 2).value = m
+            ws.cell(row=row + x, column=m + 2).value = total
+            ws.cell(row=start_row, column=m + 2).value = m
 
             #  for the graph
-            if i == 0:
+            if m == 0:
                 latest_qrt_graph.append(int(total))
-            if i == 1:
+            if m == 1:
                 lst_qrt_graph.append(int(total))
 
         if cat is not None:
@@ -259,32 +252,35 @@ def calculate_pvc(wb, masters, cat_list, start_row):
     # latest_qrt_graph = []
     # lst_qrt_graph = []
     row = start_row + 10
-    for i, m in enumerate(masters):
+    for m, master in enumerate(masters):
         hs2_total = 0
         total = 0
         other_total = 0
-        projects = list(masters[m].keys())
+        projects = list(masters[m].data.keys())
 
         for p in projects:
-            value = masters[m][p]['pvc']
+            value = masters[m].data[p]['Present Value Cost (PVC)']
             if value is not None:
-                total += value
-                if 'HS2 P' in p:
-                    hs2_total += value
-                else:
-                    other_total += value
+                try:
+                    total += value
+                    if 'HS2 P' in p:
+                        hs2_total += value
+                    else:
+                        other_total += value
+                except TypeError:
+                    pass
             else:
                 pass
 
-        if i == 0:
+        if m == 0:
             latest_qrt_graph = [int(hs2_total), int(other_total), int(total)]
-        if i == 1:
+        if m == 1:
             lst_qrt_graph = [int(hs2_total), int(other_total), int(total)]
 
-        ws.cell(row=row + 2, column=i + 2).value = hs2_total
-        ws.cell(row=row + 3, column=i + 2).value = other_total
-        ws.cell(row=row + 4, column=i + 2).value = total
-        ws.cell(row=row + 1, column=i + 2).value = m
+        ws.cell(row=row + 2, column=m + 2).value = hs2_total
+        ws.cell(row=row + 3, column=m + 2).value = other_total
+        ws.cell(row=row + 4, column=m + 2).value = total
+        ws.cell(row=row + 1, column=m + 2).value = m
 
     ws.cell(row=row + 2, column=1).value = 'HS2'
     ws.cell(row=row + 3, column=1).value = 'Other'
@@ -294,12 +290,12 @@ def calculate_pvc(wb, masters, cat_list, start_row):
                          lst_qrt_graph, 'PVC HS2 and all other projects')
 
     row = start_row + 16
-    for i, m in enumerate(masters):
+    for m, master in enumerate(masters):
         high_total = 0
         poor_total = 0
         high_total_no_hs2 = 0
         poor_total_no_hs2 = 0
-        projects = list(masters[m].keys())
+        projects = list(masters[m].data.keys())
 
         try:
             projects.remove('High Speed Rail Programme (HS2)') # stop double counting
@@ -308,32 +304,35 @@ def calculate_pvc(wb, masters, cat_list, start_row):
 
         for cat in cat_list:
             for p in projects:
-                if masters[m][p]['vfm_cat_single'] == cat:
-                    value = masters[m][p]['pvc']
+                if masters[m].data[p]["VfM Category single entry"] == cat:
+                    value = masters[m].data[p]['Present Value Cost (PVC)']
                     if value is not None:
-                        if cat in ['Poor', 'Low', 'Medium']:
-                            poor_total += value
-                            if 'HS2 P' not in p:
-                                poor_total_no_hs2 += value
-                        if cat in ['High', 'Very High']:
-                            high_total += value
-                            if 'HS2 P' not in p:
-                                high_total_no_hs2 += value
+                        try:
+                            if cat in ['Poor', 'Low', 'Medium']:
+                                poor_total += value
+                                if 'HS2 P' not in p:
+                                    poor_total_no_hs2 += value
+                            if cat in ['High', 'Very High']:
+                                high_total += value
+                                if 'HS2 P' not in p:
+                                    high_total_no_hs2 += value
+                        except TypeError:
+                            pass
                     else:
                         pass
 
-        if i == 0:
+        if m == 0:
             latest_qrt_graph = [int(poor_total), int(poor_total_no_hs2),
                                 int(high_total), int(high_total_no_hs2)]
-        if i == 1:
+        if m == 1:
             lst_qrt_graph = [int(poor_total), int(poor_total_no_hs2),
                              int(high_total), int(high_total_no_hs2)]
 
-        ws.cell(row=row + 2, column=i + 2).value = poor_total
-        ws.cell(row=row + 3, column=i + 2).value = poor_total_no_hs2
-        ws.cell(row=row + 4, column=i + 2).value = high_total
-        ws.cell(row=row + 5, column=i + 2).value = high_total_no_hs2
-        ws.cell(row=row + 1, column=i + 2).value = m
+        ws.cell(row=row + 2, column=m + 2).value = poor_total
+        ws.cell(row=row + 3, column=m + 2).value = poor_total_no_hs2
+        ws.cell(row=row + 4, column=m + 2).value = high_total
+        ws.cell(row=row + 5, column=m + 2).value = high_total_no_hs2
+        ws.cell(row=row + 1, column=m + 2).value = m
 
     ws.cell(row=row + 2, column=1).value = 'total poor-medium'
     ws.cell(row=row + 3, column=1).value = 'poor-medium excluding hs2'
--- vfm/vfm_reporting_outputs.py
+++ vfm/vfm_reporting_outputs.py
@@ -1,15 +1,13 @@
 #  code of running and compiling vfm data analysis each quarter.
 
-from data_mgmt.data import root_path
-from database.database import convert_db_python_dict, get_project_names
-from vfm.vfm_analysis_workings import compile_data_db
+from data_mgmt.data import root_path, get_master_data, get_current_project_names
+from vfm.vfm_analysis_workings import compile_data
 
-q_list = ['q1_2021', 'q4_1920']
-db_path = root_path / "core_data/vfm.db"
-master_dict = convert_db_python_dict(db_path, q_list)
-project_names = get_project_names(db_path, 'q1_2021')
+
+master_data = get_master_data()
+current_project_name_list = get_current_project_names()
 ordered_cat_list = ['Poor', 'Low', 'Medium', 'High', 'Very High',
                     'Very High and Financially Positive', 'Economically Positive',
                     None]
-run = compile_data_db(master_dict, project_names, ordered_cat_list)
-run.save(root_path / "output/vfm_data_wb_output.xlsx")
+run = compile_data(master_data, current_project_name_list, ordered_cat_list)
+run.save(root_path / "output/vfm_data_output.xlsx")
