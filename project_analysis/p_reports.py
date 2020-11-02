411ccb8e4b10d51e50333f4766a566e5ba415cd1
Will Grant <will.granty@yahoo.co.uk>
2020-11-02 11:26:57 +0000

    minor change to p_reports

--- project_analysis/p_reports.py
+++ project_analysis/p_reports.py
@@ -6,18 +6,18 @@ from datetime import date
 from typing import Dict, Union
 
 from docx import Document
-from data_mgmt.data import root_path, SRO_CONF_KEY_LIST, project_cost_profile_graph, CostData, \
+from data_mgmt.data import root_path, CostData, Projects,\
     get_master_data, Master, open_word_doc, wd_heading, key_contacts, get_project_information, \
     dca_table, dca_narratives, year_cost_profile_chart
 
 
-def compile_report(doc: Document, project_info: Dict[str, Union[str, int, date, float]], master, project) -> Document:
-    wd_heading(doc, project_info, project)
-    key_contacts(doc, master, project)
-    dca_table(doc, master, project)
-    dca_narratives(doc, master, project)
+def compile_report(doc: Document, project_info: Dict[str, Union[str, int, date, float]], master: Master, project_name: str) -> Document:
+    wd_heading(doc, project_info, project_name)
+    key_contacts(doc, master, project_name)
+    dca_table(doc, master, project_name)
+    dca_narratives(doc, master, project_name)
     costs = CostData(master)
-    costs.get_profile_project(project, 'ipdc_costs')
+    costs.get_profile_project(project_name, 'ipdc_costs')
     year_cost_profile_chart(doc, costs)
     return doc
 
@@ -26,5 +26,5 @@ wd_path = root_path / "input/summary_temp.docx"
 report_doc = open_word_doc(wd_path)
 m = Master(get_master_data(), get_project_information())
 
-output = compile_report(report_doc, get_project_information(), m, "East West Rail Configuration State 1")
-output.save(root_path / "output/ewr_cs1_report_test.docx")
+output = compile_report(report_doc, get_project_information(), m, Projects.crossrail)
+output.save(root_path / "output/crossrail_report_test.docx")
