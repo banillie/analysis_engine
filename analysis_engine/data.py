41557214a33ea513027301c4c88f4ca8005105a0
Will Grant <will.granty@yahoo.co.uk>
2021-01-18 20:44:09 +0000

    all project level analysis deleted. now in data

--- analysis_engine/data.py
+++ analysis_engine/data.py
@@ -5148,6 +5148,57 @@ def project_scope_text(doc: Document, master: Master, project_name: str) -> Docu
     return doc
 
 
+def compile_p_report(
+    doc: Document,
+    project_info: Dict[str, Union[str, int, date, float]],
+    master: Master,
+    project_name: str,
+) -> Document:
+    wd_heading(doc, project_info, project_name)
+    key_contacts(doc, master, project_name)
+    dca_table(doc, master, project_name)
+    dca_narratives(doc, master, project_name)
+    costs = CostData(master, project_name)
+    benefits = BenefitsData(master, project_name)
+    milestones = MilestoneData(master, project_name)
+    project_report_meta_data(doc, costs, milestones, benefits, project_name)
+    change_word_doc_landscape(doc)
+    cost_profile = cost_profile_graph(costs, show="No")
+    put_matplotlib_fig_into_word(doc, cost_profile)
+    total_profile = total_costs_benefits_bar_chart(costs, benefits, show="No")
+    put_matplotlib_fig_into_word(doc, total_profile)
+    #  handling of no milestones within filtered period.
+    ab = master.abbreviations[project_name]
+    try:
+        milestones.filter_chart_info(start_date="1/9/2020", end_date="30/12/2022")
+        milestones_chart = milestone_chart(
+            milestones, blue_line="ipdc_date", title=ab + " schedule (2021 - 22)", show="No"
+        )
+        put_matplotlib_fig_into_word(doc, milestones_chart)
+        # print_out_project_milestones(doc, milestones, project_name)
+    except ValueError:  # extends the time period.
+        milestones = MilestoneData(master, project_name)
+        milestones.filter_chart_info(start_date="1/9/2020", end_date="30/12/2024")
+        milestones_chart = milestone_chart(
+            milestones, blue_line="ipdc_date", title=ab + " schedule (2021 - 24)", show="No"
+        )
+        put_matplotlib_fig_into_word(doc, milestones_chart)
+    print_out_project_milestones(doc, milestones, project_name)
+    change_word_doc_portrait(doc)
+    project_scope_text(doc, master, project_name)
+    return doc
+
+
+def run_p_reports(master: Master,
+                  projects: List[str] or str) -> None:
+    projects = string_conversion(projects)
+    for p in projects:
+        report_doc = open_word_doc(root_path / "input/summary_temp.docx")
+        qrt = make_file_friendly(str(master.master_data[0].quarter))
+        output = compile_p_report(report_doc, get_project_information(), master, p)
+        output.save(root_path / "output/{}_report_{}.docx".format(p, qrt))  # add quarter here
+
+
 # TODO refactor all code below
 # def grey_conditional_formatting(ws):
 #     '''
