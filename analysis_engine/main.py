d50adc798a59971dcd4621071681f2d7ebc6014c
Will Grant <will.granty@yahoo.co.uk>
2021-07-06 16:59:59 +0100

    version test 0.0.32 added version no

--- analysis_engine/main.py
+++ analysis_engine/main.py
@@ -432,7 +432,7 @@ class main():
             formatter_class=RawTextHelpFormatter
         )
 
-        parser.add_argument('--version', action='version', version="0.0.31")
+        parser.add_argument('--version', action='version', version="0.0.32")
 
         parser.add_argument(
             'command',
