73dbe8c3f2b428a03b51ef0c59f12ea60e777af3
Will Grant <will.granty@yahoo.co.uk>
2021-08-03 15:33:18 +0100

    for version 1.0.0

--- analysis_engine/main.py
+++ analysis_engine/main.py
@@ -441,7 +441,7 @@ class main():
             formatter_class=RawTextHelpFormatter
         )
 
-        parser.add_argument('--version', action='version', version="0.0.40")
+        parser.add_argument('--version', action='version', version="1.0.0")
 
         parser.add_argument(
             'command',
